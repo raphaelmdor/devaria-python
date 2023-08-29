c60e521b5f49f49f55e1d1aee3d09227e9339dba
Raphael Monteiro de Oliveira Rebello <raphaelsiqueiraduarte@gmail.com>
2023-08-29 11:42:05 -0300

    Update main.py

--- ola-mundo/main.py
+++ ola-mundo/main.py
@@ -1,5 +1,6 @@
 def ola_mundo(name):
-    print(f'Olá mundo, sou o {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Olá mundo, sou o {name}')
 
 if __name__ == '__main__':
     ola_mundo('Raphael')
+  
